d13b8888d1cb78923f505dee4ba21accb866c04b
Matias <smatiasgm11@gmail.com>
2024-06-08 19:52:38 -0500

    Cambios en el formulario de Administrador

--- Sistema-Clinica/Administrador.Designer.cs
+++ Sistema-Clinica/Administrador.Designer.cs
@@ -30,91 +30,265 @@
         {
             this.label2 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnAgnCita = new System.Windows.Forms.Button();
-            this.btnRegistrarPac = new System.Windows.Forms.Button();
-            this.btnHabitaciones = new System.Windows.Forms.Button();
-            this.btnOperaciones = new System.Windows.Forms.Button();
+            this.btnGuardarCamb = new System.Windows.Forms.Button();
+            this.btnRealCamb = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Actividad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Turno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Encargado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FechaIni = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fechafin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtActividadNom = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEncargado = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(37, 19);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(135, 24);
+            this.label2.Size = new System.Drawing.Size(331, 25);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Vista General";
+            this.label2.Text = "Vista General para Administrador";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(41, 136);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Estado,
+            this.Actividad,
+            this.Turno,
+            this.Encargado,
+            this.FechaIni,
+            this.Fechafin});
+            this.dataGridView1.Location = new System.Drawing.Point(55, 128);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(541, 258);
+            this.dataGridView1.Size = new System.Drawing.Size(783, 317);
             this.dataGridView1.TabIndex = 2;
             // 
-            // btnAgnCita
+            // btnGuardarCamb
             // 
-            this.btnAgnCita.Location = new System.Drawing.Point(41, 71);
-            this.btnAgnCita.Margin = new System.Windows.Forms.Padding(2);
-            this.btnAgnCita.Name = "btnAgnCita";
-            this.btnAgnCita.Size = new System.Drawing.Size(118, 32);
-            this.btnAgnCita.TabIndex = 3;
-            this.btnAgnCita.Text = "Agendar Cita";
-            this.btnAgnCita.UseVisualStyleBackColor = true;
+            this.btnGuardarCamb.Location = new System.Drawing.Point(39, 510);
+            this.btnGuardarCamb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGuardarCamb.Name = "btnGuardarCamb";
+            this.btnGuardarCamb.Size = new System.Drawing.Size(124, 39);
+            this.btnGuardarCamb.TabIndex = 3;
+            this.btnGuardarCamb.Text = "Guardar cambios";
+            this.btnGuardarCamb.UseVisualStyleBackColor = true;
             // 
-            // btnRegistrarPac
+            // btnRealCamb
             // 
-            this.btnRegistrarPac.Location = new System.Drawing.Point(197, 71);
-            this.btnRegistrarPac.Margin = new System.Windows.Forms.Padding(2);
-            this.btnRegistrarPac.Name = "btnRegistrarPac";
-            this.btnRegistrarPac.Size = new System.Drawing.Size(118, 32);
-            this.btnRegistrarPac.TabIndex = 4;
-            this.btnRegistrarPac.Text = "Registrar Paciente";
-            this.btnRegistrarPac.UseVisualStyleBackColor = true;
+            this.btnRealCamb.Location = new System.Drawing.Point(39, 467);
+            this.btnRealCamb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRealCamb.Name = "btnRealCamb";
+            this.btnRealCamb.Size = new System.Drawing.Size(124, 39);
+            this.btnRealCamb.TabIndex = 6;
+            this.btnRealCamb.Text = "Realizar cambios";
+            this.btnRealCamb.UseVisualStyleBackColor = true;
             // 
-            // btnHabitaciones
+            // label1
             // 
-            this.btnHabitaciones.Location = new System.Drawing.Point(346, 71);
-            this.btnHabitaciones.Margin = new System.Windows.Forms.Padding(2);
-            this.btnHabitaciones.Name = "btnHabitaciones";
-            this.btnHabitaciones.Size = new System.Drawing.Size(118, 32);
-            this.btnHabitaciones.TabIndex = 5;
-            this.btnHabitaciones.Text = "Habitaciones";
-            this.btnHabitaciones.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Actividades en Servicios del Hospital";
             // 
-            // btnOperaciones
+            // Codigo
             // 
-            this.btnOperaciones.Location = new System.Drawing.Point(41, 431);
-            this.btnOperaciones.Margin = new System.Windows.Forms.Padding(2);
-            this.btnOperaciones.Name = "btnOperaciones";
-            this.btnOperaciones.Size = new System.Drawing.Size(118, 32);
-            this.btnOperaciones.TabIndex = 6;
-            this.btnOperaciones.Text = "Ir a Operaciones";
-            this.btnOperaciones.UseVisualStyleBackColor = true;
+            this.Codigo.HeaderText = "Cod. Actv.";
+            this.Codigo.MinimumWidth = 6;
+            this.Codigo.Name = "Codigo";
+            this.Codigo.Width = 125;
+            // 
+            // Estado
+            // 
+            this.Estado.HeaderText = "Estado";
+            this.Estado.MinimumWidth = 6;
+            this.Estado.Name = "Estado";
+            this.Estado.Width = 125;
+            // 
+            // Actividad
+            // 
+            this.Actividad.HeaderText = "Actividad";
+            this.Actividad.MinimumWidth = 6;
+            this.Actividad.Name = "Actividad";
+            this.Actividad.Width = 125;
+            // 
+            // Turno
+            // 
+            this.Turno.HeaderText = "Turno";
+            this.Turno.MinimumWidth = 6;
+            this.Turno.Name = "Turno";
+            this.Turno.Width = 125;
+            // 
+            // Encargado
+            // 
+            this.Encargado.HeaderText = "Encargado";
+            this.Encargado.MinimumWidth = 6;
+            this.Encargado.Name = "Encargado";
+            this.Encargado.Width = 125;
+            // 
+            // FechaIni
+            // 
+            this.FechaIni.HeaderText = "Fecha Inicio";
+            this.FechaIni.MinimumWidth = 6;
+            this.FechaIni.Name = "FechaIni";
+            this.FechaIni.Width = 125;
+            // 
+            // Fechafin
+            // 
+            this.Fechafin.HeaderText = "Fecha Fin";
+            this.Fechafin.MinimumWidth = 6;
+            this.Fechafin.Name = "Fechafin";
+            this.Fechafin.Width = 125;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtEncargado);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtActividadNom);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(184, 460);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(668, 116);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Estado";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Activo",
+            "Finalizado"});
+            this.comboBox1.Location = new System.Drawing.Point(74, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Nombre Actividad";
+            // 
+            // txtActividadNom
+            // 
+            this.txtActividadNom.Location = new System.Drawing.Point(139, 70);
+            this.txtActividadNom.Name = "txtActividadNom";
+            this.txtActividadNom.Size = new System.Drawing.Size(108, 22);
+            this.txtActividadNom.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(220, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Turno";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Mañana",
+            "Tarde",
+            "Noche"});
+            this.comboBox2.Location = new System.Drawing.Point(268, 15);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(101, 24);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(261, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Encargado";
+            // 
+            // txtEncargado
+            // 
+            this.txtEncargado.Location = new System.Drawing.Point(341, 70);
+            this.txtEncargado.Name = "txtEncargado";
+            this.txtEncargado.Size = new System.Drawing.Size(113, 22);
+            this.txtEncargado.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(391, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Fecha Inicio";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(484, 17);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(170, 22);
+            this.dateTimePicker1.TabIndex = 9;
             // 
             // Administrador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(660, 504);
-            this.Controls.Add(this.btnOperaciones);
-            this.Controls.Add(this.btnHabitaciones);
-            this.Controls.Add(this.btnRegistrarPac);
-            this.Controls.Add(this.btnAgnCita);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(880, 620);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRealCamb);
+            this.Controls.Add(this.btnGuardarCamb);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Administrador";
             this.Text = "Administrador";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,9 +297,26 @@
         #endregion
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnAgnCita;
-        private System.Windows.Forms.Button btnRegistrarPac;
-        private System.Windows.Forms.Button btnHabitaciones;
-        private System.Windows.Forms.Button btnOperaciones;
+        private System.Windows.Forms.Button btnGuardarCamb;
+        private System.Windows.Forms.Button btnRealCamb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Actividad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Turno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Encargado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FechaIni;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fechafin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtActividadNom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEncargado;
+        private System.Windows.Forms.Label label6;
     }
 }
--- Sistema-Clinica/Administrador.cs
+++ Sistema-Clinica/Administrador.cs
@@ -21,5 +21,10 @@ namespace Sistema_Clinica
         {
             this.Close();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
